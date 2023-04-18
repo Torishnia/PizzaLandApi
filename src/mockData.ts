9d883922da3d6259b2543d454b0aa81a4d8b334d
Torishnia <valeri.torishnya@gmail.com>
2023-04-18 17:42:43 +0300

    Modify mockData

--- src/mockData.ts
+++ src/mockData.ts
@@ -167,88 +167,156 @@ export const pizzasFromMockData = [
       }
     ]
   },
-  // {
-  //   id: 4,
-  //   title: 'Chese Overload',
-  //   description: 'Pear, cheeses: camembert, brie, blue cheese, mozzarella.',
-  //   price: 15,
-  //   image: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //   types: [0, 1],
-  //   sizes: [30, 40],
-  //   category: 2,
-  //   rating: 8,
-  //   otherImgs: [
-  //     { 
-  //       id: 1,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     },
-  //     {
-  //       id: 2,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     },
-  //     {
-  //       id: 3,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     }
-  //   ]
-  // },
-  // {
-  //   id: 5,
-  //   title: 'Neapolitan',
-  //   description: 'Cherry tomatoes, basil, sauce, cheeses: mozzarella, brie, camembert.',
-  //   price: 20,
-  //   image: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-  //   types: [1],
-  //   sizes: [25, 30],
-  //   category: 3,
-  //   rating: 7,
-  //   otherImgs: [
-  //     { 
-  //       id: 1,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-  //     },
-  //     {
-  //       id: 2,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-  //     },
-  //     {
-  //       id: 3,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-  //     }
-  //   ]
-  // },
-  // {
-  //   id: 6,
-  //   title: 'Bavarian',
-  //   description: 'Chicken breast, salami, pickled cucumbers, tomato, mayonnaise, cheese.',
-  //   price: 10,
-  //   image: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //   types: [0, 1],
-  //   sizes: [25, 30, 40],
-  //   category: 4,
-  //   rating: 7,
-  //   otherImgs: [
-  //     { 
-  //       id: 1,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     },
-  //     {
-  //       id: 2,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     },
-  //     {
-  //       id: 3,
-  //       alt: 'pizza_slider',
-  //       img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-  //     }
-  //   ]
-  // },
+  {
+    id: '4',
+    title: 'Chese Overload',
+    description: 'Pear, cheeses: camembert, brie, blue cheese, mozzarella.',
+    price: 15,
+    image: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    types: [
+      {
+        id: 1,
+        title: 'traditional',
+      },
+      {
+        id: 2,
+        title: 'thin',
+      },
+    ],
+    sizes: [
+      {
+        id: 2,
+        title: 'M',
+        size: 30,
+        coefficient: 1.5,
+      },
+      {
+        id: 3,
+        title: 'L',
+        size: 40,
+        coefficient: 2,
+      },
+    ],
+    category: 2,
+    rating: 8,
+    otherImgs: [
+      { 
+        id: 1,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+        id: 2,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+        id: 3,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2909820/pexels-photo-2909820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      }
+    ]
+  },
+  {
+    id: '5',
+    title: 'Neapolitan',
+    description: 'Cherry tomatoes, basil, sauce, cheeses: mozzarella, brie, camembert.',
+    price: 20,
+    image: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    types: [
+      {
+        id: 1,
+        title: 'traditional',
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        title: 'S',
+        size: 25,
+        coefficient: 1,
+      },
+      {
+        id: 2,
+        title: 'M',
+        size: 30,
+        coefficient: 1.5,
+      },
+    ],
+    category: 3,
+    rating: 7,
+    otherImgs: [
+      { 
+        id: 1,
+        alt: 'pizza_slider',
+        img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+      },
+      {
+        id: 2,
+        alt: 'pizza_slider',
+        img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+      },
+      {
+        id: 3,
+        alt: 'pizza_slider',
+        img: 'https://images.unsplash.com/photo-1587085416963-22efba033dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+      }
+    ]
+  },
+  {
+    id: '6',
+    title: 'Bavarian',
+    description: 'Chicken breast, salami, pickled cucumbers, tomato, mayonnaise, cheese.',
+    price: 10,
+    image: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    types: [
+      {
+        id: 1,
+        title: 'traditional',
+      },
+      {
+        id: 2,
+        title: 'thin',
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        title: 'S',
+        size: 25,
+        coefficient: 1,
+      },
+      {
+        id: 2,
+        title: 'M',
+        size: 30,
+        coefficient: 1.5,
+      },
+      {
+        id: 3,
+        title: 'L',
+        size: 40,
+        coefficient: 2,
+      },
+    ],
+    category: 4,
+    rating: 7,
+    otherImgs: [
+      { 
+        id: 1,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+        id: 2,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+        id: 3,
+        alt: 'pizza_slider',
+        img: 'https://images.pexels.com/photos/2918521/pexels-photo-2918521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      }
+    ]
+  },
 ]
