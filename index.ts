692dec8d4d9f391b88e20448a3ce600da2005463
Valeri <valeri.torishnya@gmail.com>
2023-12-19 20:34:51 +0200

    Connect to vercel

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 require('dotenv').config();
-import express from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 
 import router from './src/router';
 import { myDataSource } from './db';
 
-myDataSource
+const application = myDataSource
   .initialize()
   .then(() => {
     console.log('DataBase started');
@@ -13,10 +13,18 @@ myDataSource
   })
   .catch((err) => console.log('Something wrong with db, error: ', err.message));
 
-function start() {
+function start(): Express {
   const app = express();
   app.use(express.json());
   app.use(cors());
   app.use('/', router);
-  app.listen(process.env.APP_PORT, () => console.log(`Server started on port: ${process.env.APP_PORT}`));
+
+  // >>>> for public <<<<
+  console.log('Server starting...');
+
+  // >>>> for local development <<<<
+  // app.listen(process.env.APP_PORT, () => console.log(`Server started on port: ${process.env.APP_PORT}`));
+  return app;
 }
+
+export default application;
